933a2f39dfb1f2fc6771214bd8a443a6c1bb07a4
gycsaba <gycsaba@SZT9-24>
2024-11-22 11:09:31 +0100

    KreataInMemoryContext extension tesztadatok kezeléséhez

---
+++ Kreata/Kreata/Context/KreataInMemoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kreata.Context
+{
+    public class KreataInMemoryContext : KreataContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Seed();
+        }
+    }
+}
