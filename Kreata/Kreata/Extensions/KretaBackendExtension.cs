4a8692225ee67a7ded3b16a901ecf3edf01757d6
gycsaba <gycsaba@SZT9-24>
2024-11-22 10:25:48 +0100

    Backend extensions

---
+++ Kreata/Kreata/Extensions/KretaBackendExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Kreata.Extensions
+{
+    public static class KretaBackendExtension
+    {
+        public static void AddBackend(this IServiceCollection services)
+        { }
+
+        private static void ConfigureCors(IServiceCollection services)
+        {
+
+        }
+    }
+}
